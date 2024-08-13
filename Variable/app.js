57ad21773a98ffb86bd091b7d2015355a2129fd2
rzer682 <r.zerghani@stag.technofuturtic.education>
2024-08-13 11:49:16 +0200

    Exo sur les variable en JS/TS

---
+++ Variable/app.js
@@ -0,0 +1,12 @@
+"use strict";
+//Déclaration de variable explicit
+let machaine = "Hello World";
+//Déclarer d'une variable de typage explicite
+let nombre = 10;
+//Déclaration d'une variable pour un tableau de nombre
+let monTableau /*any[tout les type]*/ = [1, 2, 3, 4, 5];
+let monTuple = [1, true, 56, "f"];
+//Déclaration d'une variable explicit pour un objet 
+let monObjet;
+//Déclaration de variable avec le uniontype
+let monUnion = ;
---
+++ Variable/app.ts
@@ -0,0 +1,24 @@
+//Déclaration de variable explicit
+
+let machaine : string= "Hello World"
+
+//Déclarer d'une variable de typage explicite
+
+let nombre = 10
+
+//Déclaration d'une variable pour un tableau de nombre
+
+let monTableau : number[] /*any[tout les type]*/ = [1,2,3,4,5]
+
+let monTuple : [string, number, boolean] 
+
+//Déclaration d'une variable explicit pour un objet 
+
+let monObjet : {nom:string}
+
+//Déclaration de variable avec le uniontype
+
+let monUnion : string | number | undefined 
+
+//Déclaration de variable de type null 
+
